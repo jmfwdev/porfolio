3110a26e6a707e5636077e4ebc3f118a10b63c4f
JM Hore <kira@MacBook-Air.local>
2024-10-08 09:06:07 -0700

    cleaned up before start: October 8, 2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react';
 import React from "react";
-
-// components
 import AppRouter from './routers/AppRouter';
 
 // styles & scripts
 import "./styles/styles.css";
-import ParticlesComponent from "./components/Particles";
 
 function App() {
 
