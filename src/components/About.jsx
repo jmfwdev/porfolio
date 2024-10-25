7a51337dadf2eb14765ff46beb1ebec9ef10be3e
JM Hore <kira@MacBook-Air.local>
2024-10-25 13:29:15 -0700

    added more content on about and styled

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -33,34 +33,79 @@ return (
                 className="img"
         />
 
-        <p>{portfolioData.about.description}</p>
+        <p className='about-description'>{portfolioData.about.description}</p>
 
         <div className="skills">
 
             <h2>Developing Tools</h2>
 
                 <div className="skill-container developer-container">
+                    <div className='indiv-skill-container'>
+                        <img src="/assets/icons/vscode.svg" alt="vscode icon" />
+                        <p>VS Code</p>
+                    </div>
 
-                    <img src="/assets/icons/vscode.svg" alt="" />
-                    <img src="/assets/icons/html5.svg" alt="" />
-                    <img src="/assets/icons/css3.svg" alt="" />
-                    <img src="/assets/icons/sass.svg" alt="" />
-                    <img src="/assets/icons/javascript.svg" alt="" />
-                    <img src="assets/icons/react.svg" alt="" />
-                    <img src="/assets/icons/php.svg" alt="" />
-                    <img src="/assets/icons/wordpress.svg" alt="" />
-                    <img src="/assets/icons/shopify.svg" alt="" />
+                    <div className='indiv-skill-container'>
+                        <img src="/assets/icons/html5.svg" alt="html icon" />
+                        <p>HTML</p>
+                    </div>
+
+                    <div className="indiv-skill-container">
+                        <img src="/assets/icons/css3.svg" alt="css icon" />
+                        <p>CSS</p>
+                    </div>
+
+                    <div className="indiv-skill-container">
+                        <img src="/assets/icons/sass.svg" alt="sass icon" />
+                        <p>SASS</p>
+                    </div>
+
+                    <div className="indiv-skill-container">
+                        <img src="/assets/icons/javascript.svg" alt="javascript icon" />
+                        <p>Javascript</p>
+                    </div>
+
+                    <div className="indiv-skill-container">
+                        <img src="assets/icons/react.svg" alt="react icon" />
+                        <p>React JS</p>
+                    </div>
+
+                    <div className="indiv-skill-container">
+                        <img src="/assets/icons/php.svg" alt="php icon" />
+                        <p>PhP</p>
+                    </div>
+
+                    <div className="indiv-skill-container">
+                        <img src="/assets/icons/wordpress.svg" alt="Wordpress icon" />
+                        <p>Wordpress</p>
+                    </div>
+
+                    <div className="indiv-skill-container">
+                        <img src="/assets/icons/shopify.svg" alt="shopify icon" />
+                        <p>Shopify</p>
+                    </div>
 
                 </div>
 
             <h2>Designing Tools</h2>
 
                 <div className="skill-container designer-container">
-
-                    <img src="/assets/icons/photoshop.svg" alt="" />
-                    <img src="/assets/icons/illustrator.svg" alt="" />
-                    <img src= "/assets/icons/xd.svg" alt="" />
-                    <img src="/assets/icons/figma.svg" alt="" />
+                    <div className="indiv-skill-container">
+                        <img src="/assets/icons/photoshop.svg" alt="adobe-photoshop-icon" />
+                        <p>Adobe Photoshop</p>
+                    </div>
+                    <div className="indiv-skill-container">
+                        <img src="/assets/icons/illustrator.svg" alt="adobe-illustrator-icon" />
+                        <p>Adobe Illustrator</p>
+                    </div>
+                    <div className="indiv-skill-container">
+                        <img src= "/assets/icons/xd.svg" alt="adobe xd icon" />
+                        <p>Adobe XD</p>
+                    </div>
+                    <div className="indiv-skill-container">
+                        <img src="/assets/icons/figma.svg" alt="figma icon" />
+                        <p>Figma</p>
+                    </div>
 
                 </div>
 
