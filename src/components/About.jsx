f47bbd528ac8b0a33a58c9f3438171cfeaab5ffe
JM Hore <kira@MacBook-Air.local>
2024-11-11 00:35:01 -0800

    updated the about container

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,84 +39,86 @@ return (
 
             <div className="skills">
 
-                <h3>Developing Tools</h3>
+                <div className="skill-container developer-container">
 
-                    <ScrollAnimation animateIn='fadeInLeft'>
+                    <h3>Developing Tools</h3>
 
-                    <div className="skill-container developer-container">
-                        <div className='indiv-skill-container'>
-                            <img src="/assets/icons/vscode.svg" alt="vscode icon" />
-                            <p>VS Code</p>
+                        <div className="indiv-container">
+
+                            <div className='indiv-skill-container'>
+                                <img src="/assets/icons/vscode.svg" alt="vscode icon" />
+                                <p>VS Code</p>
+                            </div>
+
+                            <div className='indiv-skill-container'>
+                                <img src="/assets/icons/html5.svg" alt="html icon" />
+                                <p>HTML</p>
+                            </div>
+
+                            <div className="indiv-skill-container">
+                                <img src="/assets/icons/css3.svg" alt="css icon" />
+                                <p>CSS</p>
+                            </div>
+
+                            <div className="indiv-skill-container">
+                                <img src="/assets/icons/sass.svg" alt="sass icon" />
+                                <p>SASS</p>
+                            </div>
+
+                            <div className="indiv-skill-container">
+                                <img src="/assets/icons/javascript.svg" alt="javascript icon" />
+                                <p>Javascript</p>
+                            </div>
+
+                            <div className="indiv-skill-container">
+                                <img src="assets/icons/react.svg" alt="react icon" />
+                                <p>React JS</p>
+                            </div>
+
+                            <div className="indiv-skill-container">
+                                <img src="/assets/icons/php.svg" alt="php icon" />
+                                <p>PhP</p>
+                            </div>
+
+                            <div className="indiv-skill-container">
+                                <img src="/assets/icons/wordpress.svg" alt="Wordpress icon" />
+                                <p>Wordpress</p>
+                            </div>
+
+                            <div className="indiv-skill-container">
+                                <img src="/assets/icons/shopify.svg" alt="shopify icon" />
+                                <p>Shopify</p>
+                            </div>
+
                         </div>
-
-                        <div className='indiv-skill-container'>
-                            <img src="/assets/icons/html5.svg" alt="html icon" />
-                            <p>HTML</p>
-                        </div>
-
-                        <div className="indiv-skill-container">
-                            <img src="/assets/icons/css3.svg" alt="css icon" />
-                            <p>CSS</p>
-                        </div>
-
-                        <div className="indiv-skill-container">
-                            <img src="/assets/icons/sass.svg" alt="sass icon" />
-                            <p>SASS</p>
-                        </div>
-
-                        <div className="indiv-skill-container">
-                            <img src="/assets/icons/javascript.svg" alt="javascript icon" />
-                            <p>Javascript</p>
-                        </div>
-
-                        <div className="indiv-skill-container">
-                            <img src="assets/icons/react.svg" alt="react icon" />
-                            <p>React JS</p>
-                        </div>
-
-                        <div className="indiv-skill-container">
-                            <img src="/assets/icons/php.svg" alt="php icon" />
-                            <p>PhP</p>
-                        </div>
-
-                        <div className="indiv-skill-container">
-                            <img src="/assets/icons/wordpress.svg" alt="Wordpress icon" />
-                            <p>Wordpress</p>
-                        </div>
-
-                        <div className="indiv-skill-container">
-                            <img src="/assets/icons/shopify.svg" alt="shopify icon" />
-                            <p>Shopify</p>
-                        </div>
-
-                    </div>
-                    </ScrollAnimation>
-
-                <h3>Designing Tools</h3>
-                    <ScrollAnimation animateIn='fadeInLeft'>
-
-                    <div className="skill-container designer-container">
-                        <div className="indiv-skill-container">
-                            <img src="/assets/icons/photoshop.svg" alt="adobe-photoshop-icon" />
-                            <p>Adobe Photoshop</p>
-                        </div>
-                        <div className="indiv-skill-container">
-                            <img src="/assets/icons/illustrator.svg" alt="adobe-illustrator-icon" />
-                            <p>Adobe Illustrator</p>
-                        </div>
-                        <div className="indiv-skill-container">
-                            <img src= "/assets/icons/xd.svg" alt="adobe xd icon" />
-                            <p>Adobe XD</p>
-                        </div>
-                        <div className="indiv-skill-container">
-                            <img src="/assets/icons/figma.svg" alt="figma icon" />
-                            <p>Figma</p>
-                        </div>
-
+                        
                     </div>
 
-                    </ScrollAnimation>
+                <div className="skill-container designer-container">
 
+                    <h3>Designing Tools</h3>
+                        <div className='indiv-container'>
+
+                            <div className="indiv-skill-container">
+                                <img src="/assets/icons/photoshop.svg" alt="adobe-photoshop-icon" />
+                                <p>Adobe Photoshop</p>
+                            </div>
+                            <div className="indiv-skill-container">
+                                <img src="/assets/icons/illustrator.svg" alt="adobe-illustrator-icon" />
+                                <p>Adobe Illustrator</p>
+                            </div>
+                            <div className="indiv-skill-container">
+                                <img src= "/assets/icons/xd.svg" alt="adobe xd icon" />
+                                <p>Adobe XD</p>
+                            </div>
+                            <div className="indiv-skill-container">
+                                <img src="/assets/icons/figma.svg" alt="figma icon" />
+                                <p>Figma</p>
+                            </div>
+
+                        </div>
+
+                </div>
             </div>
         </div>
 
